2c8a1d6f20b5d2b152078f26c11c136d6c684db7
duynguyen222000 <nguyenducduy02022000@gmail.com>
2022-01-08 22:12:25 +0700

    frontend

---
+++ assets/js/swiper.js
@@ -0,0 +1,33 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+     slidesPerView: 1,
+    spaceBetween: 40,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+        },
+      },
+  });
