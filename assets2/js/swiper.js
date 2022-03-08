76e37760d71bedb4edfa8eb6d7b6c07e2b852437
duynguyen222000 <nguyenducduy02022000@gmail.com>
2022-03-08 14:01:25 +0700

    add ico-development

---
+++ assets2/js/swiper.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  loop: true,
+  slidesPerView: 1,
+  freeMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
